18d6f3bdf24de1e0626252957bb78c95ef893b3f
jan <r0th.j4n@gmail.com>
2026-02-17 21:17:46 +0100

    test: kform incidence in 3d
    Added test to check that incidence matrices are applied and are explicitly computed correctly in 3D as well. This is quite important as here more of the code is tested.

--- tests/test_kform_incidence.py
+++ tests/test_kform_incidence.py
@@ -70,19 +70,136 @@ def test_2d_derivatives(o1: int, o2: int, b1: BasisType, b2: BasisType) -> None:
     assert pytest.approx(dofs_2) == form_2.values.flatten()
 
 
-if __name__ == "__main__":
-    test_2d_derivatives(
-        1, 1, BasisType.LAGRANGE_UNIFORM, BasisType.LAGRANGE_CHEBYSHEV_GAUSS
-    )
-    for i, args in enumerate(
+@pytest.mark.parametrize(
+    ("o1", "o2", "o3", "b1", "b2", "b3"),
+    (
+        (1, 1, 1, BasisType.BERNSTEIN, BasisType.BERNSTEIN, BasisType.BERNSTEIN),
+        (3, 4, 5, BasisType.BERNSTEIN, BasisType.BERNSTEIN, BasisType.BERNSTEIN),
+        (4, 3, 5, BasisType.BERNSTEIN, BasisType.BERNSTEIN, BasisType.BERNSTEIN),
+        (3, 2, 4, BasisType.LEGENDRE, BasisType.LAGRNAGE_GAUSS, BasisType.LEGENDRE),
         (
-            (1, 1, BasisType.BERNSTEIN, BasisType.BERNSTEIN),
-            (3, 4, BasisType.BERNSTEIN, BasisType.BERNSTEIN),
-            (4, 3, BasisType.BERNSTEIN, BasisType.BERNSTEIN),
-            (3, 2, BasisType.LEGENDRE, BasisType.LEGENDRE),
-            (3, 3, BasisType.LAGRANGE_UNIFORM, BasisType.LAGRANGE_CHEBYSHEV_GAUSS),
+            3,
+            3,
+            3,
+            BasisType.LAGRANGE_UNIFORM,
+            BasisType.LAGRANGE_CHEBYSHEV_GAUSS,
+            BasisType.LAGRNAGE_GAUSS_LOBATTO,
+        ),
+    ),
+)
+def test_3d_derivatives(
+    o1: int, o2: int, o3: int, b1: BasisType, b2: BasisType, b3: BasisType
+) -> None:
+    """Check that incidence matrices for k-forms in 3D work as intended."""
+    # Init PRNG
+    rng = np.random.default_rng(
+        o1 + 2 * o2 + 3 * o3 + hash(b1) ** 2 + hash(b2) ** 2 + hash(b3) ** 2
+    )
+    # Create base function space
+    base_space = FunctionSpace(BasisSpecs(b1, o1), BasisSpecs(b2, o2), BasisSpecs(b3, o3))
+
+    # Test 0 -> 1 derivative in 3D
+    ## Create the 0-form
+    form_0 = DegreesOfFreedom(base_space)
+    form_0.values = rng.random(form_0.shape)
+
+    ## Take the derivatives
+    form_0_d0 = form_0.derivative(idim=0)
+    form_0_d1 = form_0.derivative(idim=1)
+    form_0_d2 = form_0.derivative(idim=2)
+
+    ## Try also using the incidence matrix instead
+    e01_mat = compute_kform_incidence_matrix(base_space, 0)
+    dofs_joined_01 = e01_mat @ form_0.values.flatten()
+
+    ## Check that compute values are correct
+    assert pytest.approx(dofs_joined_01[: form_0_d0.n_dofs]) == form_0_d0.values.flatten()
+    assert (
+        pytest.approx(
+            dofs_joined_01[form_0_d0.n_dofs : form_0_d0.n_dofs + form_0_d1.n_dofs]
         )
-    ):
-        print("Running test case", i, end=None)
-        test_2d_derivatives(*args)
-        print("Finished")
+        == form_0_d1.values.flatten()
+    )
+    assert (
+        pytest.approx(dofs_joined_01[form_0_d0.n_dofs + form_0_d1.n_dofs :])
+        == form_0_d2.values.flatten()
+    )
+
+    # Test 1 -> 2 derivative in 3D
+    ## Create the 1-forms
+    form_1_0 = DegreesOfFreedom(base_space.lower_order(idim=0))
+    form_1_1 = DegreesOfFreedom(base_space.lower_order(idim=1))
+    form_1_2 = DegreesOfFreedom(base_space.lower_order(idim=2))
+    form_1_0.values = rng.random(form_1_0.shape)
+    form_1_1.values = rng.random(form_1_1.shape)
+    form_1_2.values = rng.random(form_1_2.shape)
+
+    ## Take the derivatives
+    form_1_0d1 = form_1_0.derivative(idim=1)  # dy ^ dx
+    form_1_0d2 = form_1_0.derivative(idim=2)  # dz ^ dx
+    form_1_1d0 = form_1_1.derivative(idim=0)  # dx ^ dy
+    form_1_1d2 = form_1_1.derivative(idim=2)  # dz ^ dy
+    form_1_2d0 = form_1_2.derivative(idim=0)  # dx ^ dz
+    form_1_2d1 = form_1_2.derivative(idim=1)  # dy ^ dz
+
+    assert form_1_1d0.function_space == form_1_0d1.function_space
+    form_1_d0 = DegreesOfFreedom(
+        form_1_1d0.function_space, form_1_1d0.values - form_1_0d1.values
+    )
+    assert form_1_2d0.function_space == form_1_0d2.function_space
+    form_1_d1 = DegreesOfFreedom(
+        form_1_2d0.function_space, form_1_2d0.values - form_1_0d2.values
+    )
+    assert form_1_2d1.function_space == form_1_1d2.function_space
+    form_1_d2 = DegreesOfFreedom(
+        form_1_2d1.function_space, form_1_2d1.values - form_1_1d2.values
+    )
+
+    ## Try also using the incidence matrix instead
+    e12_mat = compute_kform_incidence_matrix(base_space, 1)
+    dofs_2 = e12_mat @ np.concatenate(
+        (form_1_0.values.flatten(), form_1_1.values.flatten(), form_1_2.values.flatten())
+    )
+
+    ## Check that computed values are correct
+    assert pytest.approx(dofs_2[: form_1_d0.n_dofs]) == form_1_d0.values.flatten()
+    assert (
+        pytest.approx(dofs_2[form_1_d0.n_dofs : form_1_d0.n_dofs + form_1_d1.n_dofs])
+        == form_1_d1.values.flatten()
+    )
+    assert (
+        pytest.approx(dofs_2[form_1_d0.n_dofs + form_1_d1.n_dofs :])
+        == form_1_d2.values.flatten()
+    )
+
+    # Test 2 -> 3 derivative in 3D
+    ## Create the 2-forms
+    form_2_0 = DegreesOfFreedom(base_space.lower_order(idim=0).lower_order(idim=1))
+    form_2_1 = DegreesOfFreedom(base_space.lower_order(idim=0).lower_order(idim=2))
+    form_2_2 = DegreesOfFreedom(base_space.lower_order(idim=1).lower_order(idim=2))
+    form_2_0.values = rng.random(form_2_0.shape)
+    form_2_1.values = rng.random(form_2_1.shape)
+    form_2_2.values = rng.random(form_2_2.shape)
+
+    ## Take the derivatives
+    form_2_0d2 = form_2_0.derivative(idim=2)  # dz ^ dx ^ dy (+)
+    form_2_1d1 = form_2_1.derivative(idim=1)  # dy ^ dx ^ dz (-)
+    form_2_2d0 = form_2_2.derivative(idim=0)  # dx ^ dy ^ dz (+)
+
+    assert (
+        form_2_0d2.function_space == form_2_1d1.function_space
+        and form_2_1d1.function_space == form_2_2d0.function_space
+    )
+    form_2_d = DegreesOfFreedom(
+        form_2_0d2.function_space,
+        form_2_0d2.values - form_2_1d1.values + form_2_2d0.values,
+    )
+
+    ## Try also using the incidence matrix instead
+    e23_mat = compute_kform_incidence_matrix(base_space, 2)
+    dofs_3 = e23_mat @ np.concatenate(
+        (form_2_0.values.flatten(), form_2_1.values.flatten(), form_2_2.values.flatten())
+    )
+
+    ## Check that computed values are correct
+    assert pytest.approx(dofs_3) == form_2_d.values.flatten()
